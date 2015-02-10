920b0a2fdf54d8a7cffcf1729ddd3dc04403b26f
Drew Baker <drewrb@gmail.com>
2015-02-10 11:54:07 -0800

    Updated settings descriptions

--- funkstagram-settings.php
+++ funkstagram-settings.php
@@ -30,6 +30,7 @@
 							<th scope="row"><label>Enable auto-import:</label></th>
 							<td>
 								<input name="fgram_auto" type="checkbox" id="fgram_auto" <?php checked( get_option('fgram_auto') ); ?>  value="1">
+								<p class="description">If enabled, it will import every 10 minutes.</p>																
 							</td>
 						</tr>
 						<tr valign="top">
@@ -69,14 +70,14 @@
 							<th scope="row"><label for="fgram_user_list">Users to import:</label></th>
 							<td>
 								<input name="fgram_user_list" type="text" id="fgram_user_list" value="<?php echo get_option('fgram_user_list'); ?>">
-								<p class="description">If left empty, all posts for specified tags will be imported</p>
+								<p class="description">If left empty, all posts for specified tags will be imported. Do not include @ symbol.</p>
 							</td>
 						</tr>
 						<tr valign="top">
 							<th scope="row"><label for="fgram_tag_list">Filter by tags:</label></th>
 							<td>
 								<input name="fgram_tag_list" type="text" id="fgram_tag_list" value="<?php echo get_option('fgram_tag_list'); ?>">
-								<p class="description">If left empty, all posts for specified users will be imported</p>
+								<p class="description">If left empty, all posts for specified users will be imported. Do not include # symbol.</p>
 							</td>
 						</tr>
 					</tbody>
